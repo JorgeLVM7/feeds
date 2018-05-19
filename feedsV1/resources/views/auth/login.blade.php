e6b810479223afd75e4248439b327ca181219f22
Francisco <fdzib@codevolt.com.mx>
2018-05-19 15:21:10 -0500

    desing

--- feedsV1/resources/views/auth/login.blade.php
+++ feedsV1/resources/views/auth/login.blade.php
@@ -51,11 +51,11 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary es">
                                     Entrar
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="btn btn-link es" href="{{ route('password.request') }}">
                                 ¿Olvidaste tu contraseña?
                                 </a>
                             </div>
