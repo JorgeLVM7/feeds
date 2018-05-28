7d27d813d70cc015b92e7b9f09ee372f1ed24e18
JorgeLVM7 <jorgito11@gmail.com>
2018-05-28 13:01:36 -0500

    Se agrega Mac Jorge
    Y migrations de user y roles

--- feedsV1/database/migrations/2014_10_12_000000_create_users_table.php
+++ feedsV1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,10 @@ class CreateUsersTable extends Migration
             $table->integer('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
+
+
+
+
         });
     }
 
