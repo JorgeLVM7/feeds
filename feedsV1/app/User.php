52450d2cc68768ce02805846715960b7cca584cc
JorgeLVM7 <jorgito11@gmail.com>
2018-05-28 13:22:16 -0500

    Se agrega roles_id a modelo de user

--- feedsV1/app/User.php
+++ feedsV1/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','code','active','avatar'
+        'name', 'email', 'password','code','active','avatar','roles_id_rol'
     ];
 
     /**
