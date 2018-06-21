67ce0676862766c8243c0cf0235034575f7d7415
JorgeLVM7 <jorgito11@gmail.com>
2018-06-20 19:20:36 -0500

    Se crea la vista show de restaurantes

--- feedsV1/resources/views/cPanel/restaurantes/show.blade.php
+++ feedsV1/resources/views/cPanel/restaurantes/show.blade.php
@@ -1,25 +1,88 @@
 @extends('layouts.dashboard')
 
 @section('content')
-
-
-    <div class="container">
-        <div class="row">
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-            <div class="col-6">
-                <p>hola</p>
-                <img src="../images/{{$restaurante->path}}
-                        " alt="">
-            </div>
+    <div class="row">
+        <div class="col-10 c_white">
+            <h1>Vista previa de {{ $restaurante->nombre }}</h1>
         </div>
+        <div class="col-2">
+            <a class="btn btn-light float-right" href="{{route('restaurantes.index')}}">Regresar</a>
+
+        </div>
+    </div>
+    <div class="row">
+        <br>
+    </div>
+
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Nombre de restaurante:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> nombre }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Descripción:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> descripcion }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Sitio Web:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> sitio_web }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Calle:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> calle }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5># interior:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> no_int }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5># exterior:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> no_ext }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Colonia:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> colonia }}</h1>
+        </div>
+    </div>
+    <div class="row c_white">
+        <div class="col-3">
+            <h5>Código Postal:</h5>
+        </div>
+        <div class="col-9">
+            <h1>{{$restaurante -> codigo_postal }}</h1>
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row">
+
     </div>
 
 @endsection
