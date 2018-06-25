38a627d6dd7e0f29a694a020c6e650b332f73013
Francisco <fdzib@codevolt.com.mx>
2018-06-25 11:42:32 -0500

    cambios

--- feedsV1/app/Http/Controllers/HomeController.php
+++ feedsV1/app/Http/Controllers/HomeController.php
@@ -25,9 +25,9 @@ class HomeController extends Controller
     public function index()
     {
 
-        $promociones = Promocion::orderBy('id', 'DESC');
+        $promociones = Promocion::orderBy('id', 'DESC')->paginate();
 
-        $eventos = Evento::orderBy('id', 'DESC');
+        $eventos = Evento::orderBy('id', 'DESC')->paginate();
 
 
         return view('cPanel.products.index',compact('promociones','eventos'));
--- feedsV1/app/Http/Controllers/ProductsController.php
+++ feedsV1/app/Http/Controllers/ProductsController.php
@@ -11,9 +11,9 @@ class ProductsController extends Controller
     public function index()
     {
 
-        $promociones = Promocion::orderBy('id', 'DESC');
+        $promociones = Promocion::orderBy('id', 'DESC')->paginate();
 
-        $eventos = Evento::orderBy('id', 'DESC');
+        $eventos = Evento::orderBy('id', 'DESC')->paginate();
 
 
         return view('cPanel.products.index',compact('promociones','eventos'));
