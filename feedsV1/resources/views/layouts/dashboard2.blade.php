d3a0c1d4afb58007ac08c8bb44d448ab49333025
JorgeLVM7 <jorgito11@gmail.com>
2018-07-18 18:58:04 -0500

    act

--- feedsV1/resources/views/layouts/dashboard2.blade.php
+++ feedsV1/resources/views/layouts/dashboard2.blade.php
@@ -79,11 +79,26 @@
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDd9EjNxKWJXePrvVy342SA4-WqdnjzJ74&libraries=places">
 </script>
 
-@yield('script')
-
 {{--Select2 script from CDN--}}
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+
+@yield('script')
+
+<<<<<<< HEAD
+{{--Select2 script from CDN--}}
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+=======
+<!--{{--Opcion --}}
+
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>--}}
+{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />--}}
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>--}}-->
+>>>>>>> ec8631c7f63d91e2818b74e46af60262b61269a4
 
 
 </body>
