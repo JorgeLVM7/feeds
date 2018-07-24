56c9cc6aecf1686e9ec6ab2ba13dc0f7636835e2
JorgeLVM7 <jorgito11@gmail.com>
2018-07-24 13:31:53 -0500

    migration de restaurantes completa

--- feedsV1/database/migrations/2018_07_24_162543_add_column_user_id_to_restaurantes.php
+++ feedsV1/database/migrations/2018_07_24_162543_add_column_user_id_to_restaurantes.php
@@ -20,21 +20,25 @@ class AddColumnUserIdToRestaurantes extends Migration
             $table->increments('id');
             $table->string('nombre');
             $table->string('descripcion');
-            $table->string('sitio_web');
+            $table->string('sitio_web')->nullable();
             $table->integer('idcategoria1');
-            $table->integer('idcategoria2');
-            $table->integer('idcategoria3');
+            $table->integer('idcategoria2')->nullable();
+            $table->integer('idcategoria3')->nullable();
             $table->string('email')->unique();
             $table->string('calle');
-            $table->integer('no_int');
-            $table->integer('no_ext');
+            $table->integer('no_int')->nullable();
+            $table->integer('no_ext')->nullable();
             $table->string('colonia');
             $table->integer('codigo_postal');
             $table->string('referencia');
-            $table->integer('telefono');
+            $table->bigInteger('telefono');
             $table->string('horario');
+
+            $table->string('hora1');
+            $table->string('hora2');
             $table->string('latitud');
             $table->string('longitud');
+
             $table->string('RFC');
             $table->string('path')->nullable();
 
