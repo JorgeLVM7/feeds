0bcc7e6fa258c0c080e87209ac3bdbbb18ed3035
JorgeLVM7 <jorgito11@gmail.com>
2018-07-26 16:28:27 -0500

    Cambio de fondo

--- feedsV1/resources/views/layouts/mapa.blade.php
+++ feedsV1/resources/views/layouts/mapa.blade.php
@@ -24,7 +24,7 @@
   	</style>
     <title>Feeds | Mapa </title>
 </head>
-<body>
+<body class="fondo">
 <nav class="navbar navbar-expand-lg menu fixed-top" >
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
